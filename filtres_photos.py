c5fe749e3248f59b1548fd1af8e2eed896b1c3c0
Kevin <godec.kevin@gmail.com>
2013-01-17 13:16:51 +0100

    ajout de trucs

--- filtres_photos.py
+++ filtres_photos.py
@@ -1,9 +1,11 @@
 from PIL import Image
 import ImageFilter
 import time
-im = Image.open("lena.bmp")
+import random
+im = Image.open("tour.jpg")
+p = im.load()
 
-
+#////////////////////////  FONCTIONS ANNEXES  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 def contraste(im,valR=1.0,valG=1.0,valB=1.0):
     imtmp=Image.new(im.mode ,im.size)
@@ -11,13 +13,64 @@ def contraste(im,valR=1.0,valG=1.0,valB=1.0):
     if im.mode=="RGB":
         pixels = [ (int(min(max(128+(pixel[0]-128)*valR,0),255)),int(min(max(128+(pixel[1]-128)*valG,0),255)),int(min(max(128+(pixel[2]-128)*valB,0),255))) for pixel in pixels]
     else:
-        pixels = [ int(min(max(128+(pixel-128)*valR,0),255)) for pixel in pixels]
-            
+        pixels = [ int(min(max(128+(pixel-128)*valR,0),255)) for pixel in pixels]    
     imtmp.putdata(pixels)
     return imtmp
 
-contraste(im,7,2,0.6).show()
-Image.blend(contraste(im,7,2,0.6), im.filter(ImageFilter.CONTOUR), 0.2).filter(ImageFilter.SMOOTH_MORE).show()
+def luminosite(im,val):
+    imtmp=Image.new(im.mode ,im.size)
+    pixels = list(im.getdata())
+    if im.mode=="RGB":
+        pixels = [ (int(min(max(pixel[0]+val,0),255)),int(min(max(pixel[1]+val,0),255)),int(min(max(pixel[2]+val,0),255))) for pixel in pixels]
+    else:
+        pixels = [ int(min(max(pixel+val,0),255)) for pixel in pixels]    
+    imtmp.putdata(pixels)
+    return imtmp
+
+def sepia(im):
+    imtmp=Image.new("RGB" ,im.size)
+    pixels = list(im.getdata())
+    if im.mode=="RGB":
+        pixels = [ (int(min(max((pixel[0] * .393) + (pixel[1] *.769) + (pixel[2]  * .189),0),255)),
+                        int(min(max((pixel[0] * .349) + (pixel[1] *.686) + (pixel[2]  * .168),0),255)),
+                        int(min(max((pixel[0] * .272) + (pixel[1] *.534) + (pixel[2]  * .131),0),255))) for pixel in pixels]
+    else:
+        pixels = [ (int(min(max((pixel * .393) + (pixel *.769) + (pixel  * .189),0),255)),
+                        int(min(max((pixel * .349) + (pixel *.686) + (pixel  * .168),0),255)),
+                        int(min(max((pixel * .272) + (pixel *.534) + (pixel  * .131),0),255))) for pixel in pixels]
+    imtmp.putdata(pixels)
+    return imtmp
+
+def bruit(im,var):
+    imtmp=Image.new(im.mode ,im.size)
+    pixels = list(im.getdata())
+    if im.mode=="RGB":
+        pixels = [ (int(min(max(pixel[0]*random.normalvariate(1,var),0),255)),int(min(max(pixel[1]*random.normalvariate(1,var),0),255)),int(min(max(pixel[2]*random.normalvariate(1,var),0),255))) for pixel in pixels]
+    else:
+        pixels = [ int(min(max(pixel*random.normalvariate(1,var),0),255)) for pixel in pixels]    
+    imtmp.putdata(pixels)
+    return imtmp
+
+#////////////////////////  LISTE DE FILTRES  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+def test1(im):
+    imtmp = Image.blend(contraste(im,1,1.5,2), im.filter(ImageFilter.SMOOTH), 0.2).filter(ImageFilter.SMOOTH_MORE)
+    return imtmp
+
+def filtre_vieux(im):
+    imtmp = sepia(luminosite(bruit(contraste(im,0.3,0.3,0.3),0.15).filter(ImageFilter.SMOOTH_MORE),20))
+    return imtmp
+
+
+
+
+#////////////////////////  TESTS  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+filtre_vieux(im).show()
+    
+
 
             
     
