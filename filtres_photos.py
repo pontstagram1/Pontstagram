f32bbe832cc5c8aa9e815ff6fc8035c9b9c59b9a
Kevin <godec.kevin@gmail.com>
2013-01-29 11:56:19 +0100

    des supers filtres

--- filtres_photos.py
+++ filtres_photos.py
@@ -53,13 +53,13 @@ def bruit(im,var):
     return imtmp
 
 def flou_pixel(pixel,pixel2,x,y,w,h,a,b,c):
-    if (x>0) and (x<w-1) and (y>0) and (y<h-1):
+    if (x>1) and (x<w-2) and (y>1) and (y<h-2):
         if im.mode =="RGB":
-            pixel2[x,y]=(int(a*pixel[x,y][0]+(b*0.25)*(pixel[x-1,y][0]+pixel[x+1,y][0]+pixel[x,y-1][0]+pixel[x,y+1][0])+(c*0.25)*(pixel[x-1,y-1][0]+pixel[x-1,y+1][0]+pixel[x+1,y-1][0]+pixel[x+1,y+1][0])),
-                        int(a*pixel[x,y][1]+(b*0.25)*(pixel[x-1,y][1]+pixel[x+1,y][1]+pixel[x,y-1][1]+pixel[x,y+1][1])+(c*0.25)*(pixel[x-1,y-1][1]+pixel[x-1,y+1][1]+pixel[x+1,y-1][1]+pixel[x+1,y+1][1])),
-                        int(a*pixel[x,y][2]+(b*0.25)*(pixel[x-1,y][2]+pixel[x+1,y][2]+pixel[x,y-1][2]+pixel[x,y+1][2])+(c*0.25)*(pixel[x-1,y-1][2]+pixel[x-1,y+1][2]+pixel[x+1,y-1][2]+pixel[x+1,y+1][2])))
+            pixel2[x,y]=(int(a*pixel[x,y][0]+(b*0.25)*(pixel[x-2,y][0]+pixel[x+2,y][0]+pixel[x,y-2][0]+pixel[x,y+2][0])+(c*0.25)*(pixel[x-2,y-2][0]+pixel[x-2,y+2][0]+pixel[x+2,y-2][0]+pixel[x+2,y+2][0])),
+                        int(a*pixel[x,y][1]+(b*0.25)*(pixel[x-2,y][1]+pixel[x+2,y][1]+pixel[x,y-2][1]+pixel[x,y+2][1])+(c*0.25)*(pixel[x-2,y-2][1]+pixel[x-2,y+2][1]+pixel[x+2,y-2][1]+pixel[x+2,y+2][1])),
+                        int(a*pixel[x,y][2]+(b*0.25)*(pixel[x-2,y][2]+pixel[x+2,y][2]+pixel[x,y-2][2]+pixel[x,y+2][2])+(c*0.25)*(pixel[x-2,y-2][2]+pixel[x-2,y+2][2]+pixel[x+2,y-2][2]+pixel[x+2,y+2][2])))
         else :
-            pixel2[x,y]=a*pixel[x,y]+(b/4)*(pixel[x-1,y]+pixel[x+1,y]+pixel[x,y-1]+pixel[x,y+1])+(c/4)*(pixel[x-1,y-1]+pixel[x-1,y+1]+pixel[x+1,y-1]+pixel[x+1,y+1])
+            pixel2[x,y]=a*pixel[x,y]+(b/4)*(pixel[x-2,y]+pixel[x+2,y]+pixel[x,y-2]+pixel[x,y+2])+(c/4)*(pixel[x-2,y-2]+pixel[x-2,y+2]+pixel[x+2,y-2]+pixel[x+2,y+2])
 
 def flou(im,sigma):
     pixel = im.load()
@@ -78,65 +78,79 @@ def flou(im,sigma):
             flou_pixel(pixel,pixel2,x,y,W,H,a,b,c)
     return imtmp
 
-def flou2(im,sigma):
+   
+def flou_decroissant(im,sigma):
     pixel = im.load()
     imtmp=Image.new("RGB" ,im.size)
     pixel2 = imtmp.load()
     W,H = im.size
-    t=min(W,H)/3
-    a=1
-    b= exp(-1/(2*sigma))
-    c=  exp(-2/(2*sigma))
+    milieuW=W/2
+    milieuH=H/2
     for x in range(W):
         for y in range(H):
-            if ((x-W/2)*(x-W/2)+(y-H/2)*(y-H/2)>t*t):
-                flou_pixel(pixel,pixel2,x,y,W,H,a,b,c)
-            else:
-                pixel2[x,y]=pixel[x,y]
-    return imtmp           
-    
-    
+            sigma2 = (abs(x-milieuW)+abs(y-milieuH))*sigma/(milieuW+milieuH)+0.01
+            a= 1
+            b= exp(-1/float(2*sigma2))
+            c=  exp(-1/float(sigma2))
+            norm = a+b+c
+            a=a/norm
+            b=b/norm
+            c=c/norm
+            flou_pixel(pixel,pixel2,x,y,W,H,a,b,c)
+    return imtmp
 
+def cadre_polaroid(im):
+    imtmp=im.copy()
+    pixel = imtmp.load()
+    W,H = im.size
+    limite = max(W,H)/20
+    limite2 = limite/2
+    for x in range(W):
+        for y in range(limite2):
+            pixel[x,y]=(254,254,226)
+    for x in range(W):
+        for y in range(limite2):
+            pixel[x,y]=(254,254,226)
+    for x in range(limite2):
+        for y in range(H):
+            pixel[x,y]=(254,254,226)
+            pixel[W-1-x,y]=(254,254,226)
+    for x in range(W):
+        for y in range(limite):
+            pixel[x,H-1-y]=(254,254,226)             
+    return imtmp
+    
         
 
 #////////////////////////  LISTE DE FILTRES  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+def binary(im):
+    imtmp=im.copy()
+    pixel =imtmp.load()
+    X,Y = imtmp.size
+    if im.mode=="RGB":
+        for x in range(X):
+            for y in range(Y):
+                pixel[x,y]=((pixel[x,y][0]>128)*255,(pixel[x,y][1]>128)*255,(pixel[x,y][2]>128)*255)
+    else:
+        for x in range(X):
+            for y in range(Y):
+                pixel[x,y]=(pixel[x,y]>128)*255
+    return imtmp.filter(ImageFilter.SMOOTH_MORE).filter(ImageFilter.SMOOTH_MORE)
 
 
-def test1(im):
+def nostalgie(im):
     imtmp = Image.blend(contraste(im,1,1.5,2), im.filter(ImageFilter.SMOOTH), 0.2).filter(ImageFilter.SMOOTH_MORE)
     return imtmp
 
-def test2(im):
-    imtmp = Image.blend(contraste(im,2,1.5,0.7), im.filter(ImageFilter.SMOOTH), 0.2).filter(ImageFilter.SMOOTH_MORE)
-    return imtmp
+def aube(im):
+    imtmp = Image.blend(contraste(im,2,1.5,0.7), im.filter(ImageFilter.SMOOTH), 0.2)
+    return flou_decroissant(imtmp,2)
+
+def printemps_nucleaire(im):
+    return cadre_polaroid(contraste(im,1.5,0.7,2).filter(ImageFilter.SMOOTH_MORE))
 
 def filtre_vieux(im):
-    imtmp = sepia(luminosite(bruit(contraste(im,0.3,0.3,0.3).filter(ImageFilter.SMOOTH_MORE),0.15),20))
-    return imtmp
-
-def filtre_vieux_opt(im):
-    imtmp=Image.new("RGB" ,im.size)
-    pixels = list(im.getdata())
-    pixelstmp=list()
-    if im.mode=="RGB":
-        for pixel in pixels:            
-            p0=((128+(pixel[0]-128)*0.3)*random.normalvariate(1,0.15)+20)
-            p1=((128+(pixel[1]-128)*0.3)*random.normalvariate(1,0.15)+20)
-            p2=((128+(pixel[2]-128)*0.3)*random.normalvariate(1,0.15)+20)
-            pixelstmp.append( (int(min(max(p0 * .393 + p1 *.769+ p2 * .189,0),255)),
-                        int(min(max(p0 * .349 + p1*.686 + p2* .168,0),255)),
-                        int(min(max(p0* .272 + p1*.534 + p2* .131,0),255))))
-    else:
-        for pixel in pixels:
-            p0=((128+(pixel-128)*0.3)*random.normalvariate(1,0.15)+20)
-            pixelstmp.append((int(min(max(p0 * (.393 +.769+.189),0),255)),
-                        int(min(max(p0 * (.349 +.686 + .168),0),255)),
-                        int(min(max(p0* (.272 +.534 + .131),0),255))))
-    imtmp.putdata(pixelstmp)
-    return imtmp.filter(ImageFilter.SMOOTH_MORE)
-
-def filtre_vieux_opt2(im):
     imtmp=im.copy()
     pixel =imtmp.load()
     X,Y = imtmp.size
@@ -157,13 +171,58 @@ def filtre_vieux_opt2(im):
                         int(min(max(p0* (.272 +.534 + .131),0),255))))
     return imtmp.filter(ImageFilter.SMOOTH_MORE)
     
+def amour_gloire_et_beaute(im):
+    imtmp=im.copy()
+    pixel = imtmp.load()
+    W,H = im.size
+    milieuW=W/2
+    milieuH=H/2
+    if im.mode=="RGB":
+        for x in range(W):
+            for y in range(H):
+                valR=(1-(abs(x-milieuW)+abs(y-milieuH))/float((milieuW+milieuH)))*0.9
+                valG=valR
+                valB=valG
+                pixel[x,y] = (int(min(max(128+(pixel[x,y][0]-128)*valR,0),255)),int(min(max(128+(pixel[x,y][1]-128)*valG,0),255)),int(min(max(128+(pixel[x,y][2]-128)*valB,0),255))) 
+    else:
+        for x in range(W):
+            for y in range(H):
+                valR=(1-(abs(x-milieuW)+abs(y-milieuH))/float((milieuW+milieuH)))*0.9
+                pixel[x,y] = int(min(max(128+(pixel[x,y]-128)*valR,0),255))
+    return imtmp
 
-
-
+def amaro(im):
+    imtmp=im.copy()
+    pixel = imtmp.load()
+    W,H = im.size
+    milieuW=W/2
+    milieuH=H/2
+    if im.mode=="RGB":
+        for x in range(W):
+            for y in range(H):
+                tmp=pow((abs(x-milieuW)+abs(y-milieuH))/float((milieuW+milieuH)),2.5)+0.1
+                valR=3*tmp+0.7
+                valB=0.4*tmp+0.7
+                valG=3*tmp+0.7
+                pixel[x,y] = (int(min(max(90+(pixel[x,y][0]-128)*valR,0),255)),int(min(max(90+(pixel[x,y][1]-128)*valG,0),255)),int(min(max(90+(pixel[x,y][2]-128)*valB,0),255))) 
+    else:
+        for x in range(W):
+            for y in range(H):
+                valR=(1-(abs(x-milieuW)+abs(y-milieuH))/float((milieuW+milieuH)))*0.9
+                pixel[x,y] = int(min(max(108+(pixel[x,y]-128)*valR,0),255))
+    return flou_decroissant(imtmp,2)
 
 #////////////////////////  TESTS  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-#im = Image.open("paysage.jpg")
-#flou(im,5).show()
+
+
+#im = Image.open("jungle.jpg")
+#aube(im).show()
+#nostalgie(im).show()
+#binary(im).show()
+#amaro(im).show()
+#filtre_vieux(im).show()
+#amour_gloire_et_beaute(im).show()
+#printemps_nucleaire(im).show()
 
 
 
