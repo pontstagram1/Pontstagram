7060c7fbe50fa907c08e12a5f62011986a255566
Kevin <godec.kevin@gmail.com>
2013-01-29 09:07:08 +0100

    c'est bon!

--- filtres_photos.py
+++ filtres_photos.py
@@ -2,8 +2,9 @@ from PIL import Image
 import ImageFilter
 import time
 import random
-im = Image.open("exemple.jpg")
-p = im.load()
+from math import exp,pi,sqrt
+im = Image.open("paysage.jpg")
+
 
 #////////////////////////  FONCTIONS ANNEXES  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
@@ -51,6 +52,53 @@ def bruit(im,var):
     imtmp.putdata(pixels)
     return imtmp
 
+def flou_pixel(pixel,pixel2,x,y,w,h,a,b,c):
+    if (x>0) and (x<w-1) and (y>0) and (y<h-1):
+        if im.mode =="RGB":
+            pixel2[x,y]=(int(a*pixel[x,y][0]+(b*0.25)*(pixel[x-1,y][0]+pixel[x+1,y][0]+pixel[x,y-1][0]+pixel[x,y+1][0])+(c*0.25)*(pixel[x-1,y-1][0]+pixel[x-1,y+1][0]+pixel[x+1,y-1][0]+pixel[x+1,y+1][0])),
+                        int(a*pixel[x,y][1]+(b*0.25)*(pixel[x-1,y][1]+pixel[x+1,y][1]+pixel[x,y-1][1]+pixel[x,y+1][1])+(c*0.25)*(pixel[x-1,y-1][1]+pixel[x-1,y+1][1]+pixel[x+1,y-1][1]+pixel[x+1,y+1][1])),
+                        int(a*pixel[x,y][2]+(b*0.25)*(pixel[x-1,y][2]+pixel[x+1,y][2]+pixel[x,y-1][2]+pixel[x,y+1][2])+(c*0.25)*(pixel[x-1,y-1][2]+pixel[x-1,y+1][2]+pixel[x+1,y-1][2]+pixel[x+1,y+1][2])))
+        else :
+            pixel2[x,y]=a*pixel[x,y]+(b/4)*(pixel[x-1,y]+pixel[x+1,y]+pixel[x,y-1]+pixel[x,y+1])+(c/4)*(pixel[x-1,y-1]+pixel[x-1,y+1]+pixel[x+1,y-1]+pixel[x+1,y+1])
+
+def flou(im,sigma):
+    pixel = im.load()
+    imtmp=Image.new("RGB" ,im.size)
+    pixel2 = imtmp.load()
+    W,H = im.size
+    a= 1
+    b= exp(-1/float(2*sigma))
+    c=  exp(-1/float(sigma))
+    norm = a+b+c
+    a=a/norm
+    b=b/norm
+    c=c/norm
+    for x in range(W):
+        for y in range(H):
+            flou_pixel(pixel,pixel2,x,y,W,H,a,b,c)
+    return imtmp
+
+def flou2(im,sigma):
+    pixel = im.load()
+    imtmp=Image.new("RGB" ,im.size)
+    pixel2 = imtmp.load()
+    W,H = im.size
+    t=min(W,H)/3
+    a=1
+    b= exp(-1/(2*sigma))
+    c=  exp(-2/(2*sigma))
+    for x in range(W):
+        for y in range(H):
+            if ((x-W/2)*(x-W/2)+(y-H/2)*(y-H/2)>t*t):
+                flou_pixel(pixel,pixel2,x,y,W,H,a,b,c)
+            else:
+                pixel2[x,y]=pixel[x,y]
+    return imtmp           
+    
+    
+
+        
+
 #////////////////////////  LISTE DE FILTRES  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 
@@ -59,17 +107,63 @@ def test1(im):
     imtmp = Image.blend(contraste(im,1,1.5,2), im.filter(ImageFilter.SMOOTH), 0.2).filter(ImageFilter.SMOOTH_MORE)
     return imtmp
 
-def filtre_vieux(im):
-    imtmp = sepia(luminosite(bruit(contraste(im,0.3,0.3,0.3),0.15).filter(ImageFilter.SMOOTH_MORE),20))
+def test2(im):
+    imtmp = Image.blend(contraste(im,2,1.5,0.7), im.filter(ImageFilter.SMOOTH), 0.2).filter(ImageFilter.SMOOTH_MORE)
     return imtmp
+
+def filtre_vieux(im):
+    imtmp = sepia(luminosite(bruit(contraste(im,0.3,0.3,0.3).filter(ImageFilter.SMOOTH_MORE),0.15),20))
+    return imtmp
+
+def filtre_vieux_opt(im):
+    imtmp=Image.new("RGB" ,im.size)
+    pixels = list(im.getdata())
+    pixelstmp=list()
+    if im.mode=="RGB":
+        for pixel in pixels:            
+            p0=((128+(pixel[0]-128)*0.3)*random.normalvariate(1,0.15)+20)
+            p1=((128+(pixel[1]-128)*0.3)*random.normalvariate(1,0.15)+20)
+            p2=((128+(pixel[2]-128)*0.3)*random.normalvariate(1,0.15)+20)
+            pixelstmp.append( (int(min(max(p0 * .393 + p1 *.769+ p2 * .189,0),255)),
+                        int(min(max(p0 * .349 + p1*.686 + p2* .168,0),255)),
+                        int(min(max(p0* .272 + p1*.534 + p2* .131,0),255))))
+    else:
+        for pixel in pixels:
+            p0=((128+(pixel-128)*0.3)*random.normalvariate(1,0.15)+20)
+            pixelstmp.append((int(min(max(p0 * (.393 +.769+.189),0),255)),
+                        int(min(max(p0 * (.349 +.686 + .168),0),255)),
+                        int(min(max(p0* (.272 +.534 + .131),0),255))))
+    imtmp.putdata(pixelstmp)
+    return imtmp.filter(ImageFilter.SMOOTH_MORE)
+
+def filtre_vieux_opt2(im):
+    imtmp=im.copy()
+    pixel =imtmp.load()
+    X,Y = imtmp.size
+    if im.mode=="RGB":
+        for x in range(X):
+            for y in range(Y):
+                p0=((128+(pixel[x,y][0]-128)*0.3)*random.normalvariate(1,0.15)+20)
+                p1=((128+(pixel[x,y][1]-128)*0.3)*random.normalvariate(1,0.15)+20)
+                p2=((128+(pixel[x,y][2]-128)*0.3)*random.normalvariate(1,0.15)+20)
+                pixel[x,y]=( (int(min(max(p0 * .393 + p1 *.769+ p2 * .189,0),255)),
+                            int(min(max(p0 * .349 + p1*.686 + p2* .168,0),255)),
+                            int(min(max(p0* .272 + p1*.534 + p2* .131,0),255))))
+    else:
+        for pixel in pixels:
+            p0=((128+(pixel-128)*0.3)*random.normalvariate(1,0.15)+20)
+            pixelstmp.append((int(min(max(p0 * (.393 +.769+.189),0),255)),
+                        int(min(max(p0 * (.349 +.686 + .168),0),255)),
+                        int(min(max(p0* (.272 +.534 + .131),0),255))))
+    return imtmp.filter(ImageFilter.SMOOTH_MORE)
+    
 
 
 
 
 #////////////////////////  TESTS  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#flou(im,5).show()
 
-#filtre_vieux(im).show()
-    
 
 
             
